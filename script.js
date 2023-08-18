1500c0720aea60c1afb726a5bc0b1cf77147aeac
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2023-08-18 15:38:15 +0100

    added code in js file to remove previous table

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function gerar(){
     var numero = Number(num.value);
     var caixa = document.getElementById("box");
 
-    
+    function removeOpts(selectElement){
+        var i, L = selectElement.options.length - 1;
+        for(i = L; i >= 0; i--){
+            selectElement.remove(i);
+        }
+    }
 
     if(num.value.length == 0){
         window.alert('Digite um numero e tente novamente');
