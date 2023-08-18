a390f0c9f9814870b0d9e870b886b6aa1b69cb03
Andre Cruz <71467368+andremscruz@users.noreply.github.com>
2023-08-18 17:02:01 +0100

    added another bix to include exponents

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function gerar(){
     var num = document.getElementById("num");
     var numero = Number(num.value);
     var caixa = document.getElementById("box");
+    var caixa2 = document.getElementById("box2");
 
     function removeOpts(selectElement){
         var i, L = selectElement.options.length - 1;
@@ -16,11 +17,18 @@ function gerar(){
     }
     else{
         removeOpts(caixa);
+        removeOpts(caixa2);
         for(var i = 0; i <= 10; i++){
         var opt = document.createElement('option');
         opt.value = i;
         opt.innerHTML = `${numero} x ${i} = ${numero*i}`;
         caixa.appendChild(opt);
+        }
+        for(var i = 0; i <=10; i++){
+            var opt2 = document.createElement('option');
+            opt2.value = i;
+            opt2.innerHTML =`${numero} exponent ${i} = ${numero ** i}`; //Math.pow(b, e)
+            caixa2.appendChild(opt2); 
         }
     }
 }
